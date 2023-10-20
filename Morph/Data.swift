04fca52caba229e972b7670a237bfc7d1f5ef7f8
Marzia Pirozzi <94360287+Marziap@users.noreply.github.com>
2023-10-20 15:19:10 +0200

    Update Data.swift

--- Morph/Data.swift
+++ Morph/Data.swift
@@ -1,8 +1,23 @@
 //
 //  Data.swift
-//  Morph
+//  ProvaMusic
 //
-//  Created by Marzia Pirozzi on 20/10/23.
+//  Created by Marzia Pirozzi on 17/10/23.
 //
 
 import Foundation
+import SwiftUI
+
+class Data: ObservableObject{
+    @Published var sounds:[Sounds] = [
+        Sounds(name: "Sound 1", color: .green),
+        Sounds(name: "Sound 2", color: .yellow),
+        Sounds(name: "Sound 3", color: .red),
+        Sounds(name: "Sound 4", color: .teal),
+        Sounds(name: "Sound 5", color: .orange)
+    ]
+    
+    @Published var mixSounds: [Sounds] = []
+}
+
+var data = Data()
