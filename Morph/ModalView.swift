d9570727dcea358c83ff554599c24f7756d57c36
Marzia Pirozzi <94360287+Marziap@users.noreply.github.com>
2023-10-20 15:19:48 +0200

    Update ModalView.swift

--- Morph/ModalView.swift
+++ Morph/ModalView.swift
@@ -1,15 +1,39 @@
 //
 //  ModalView.swift
-//  Morph
+//  ProvaMusic
 //
-//  Created by Marzia Pirozzi on 20/10/23.
+//  Created by Marzia Pirozzi on 17/10/23.
 //
 
 import SwiftUI
 
 struct ModalView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading){
+            Text("Your sounds")
+                .font(.largeTitle)                  .padding(.vertical, 50)
+            
+            ScrollView{
+                ForEach(data.sounds){sound in
+                    Button {
+                        data.mixSounds.append(sound)
+                        print(data.mixSounds[0].name)
+                    } label: {
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(Color(red: 0.87, green: 0.87, blue: 0.87))
+                                .frame(width: 188, height: 30)
+                                .cornerRadius(10)
+                            
+                            Text(sound.name)
+                                .foregroundColor(.black)
+                        }
+                    }.padding(10)
+                }
+            }
+            
+        }
+        
     }
 }
 
