380784ea9d2c16ba080889c3e9ca422e721b0711
Marzia Pirozzi <94360287+Marziap@users.noreply.github.com>
2023-10-20 15:19:33 +0200

    Update MixingView.swift

--- Morph/MixingView.swift
+++ Morph/MixingView.swift
@@ -1,17 +1,116 @@
 //
 //  MixingView.swift
-//  Morph
+//  ProvaMusic
 //
-//  Created by Marzia Pirozzi on 20/10/23.
+//  Created by Marzia Pirozzi on 17/10/23.
 //
 
 import SwiftUI
 
+
 struct MixingView: View {
+    @State private var sounds:[Sounds] = []
+    @State private var showModal = false
+    
+    func move(from source: IndexSet, to destination: Int) {
+            sounds.move(fromOffsets: source, toOffset: destination)
+        data.mixSounds=sounds
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Mixing")
+                .font(.largeTitle)
+                .padding(.bottom, 50)
+            HStack{
+                 
+                    if(!sounds.isEmpty){
+                        Image(systemName: "play")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 30)
+                            .foregroundColor(.green)
+                            .padding(.horizontal)
+                    }
+                
+                ForEach(sounds) { sound in
+                
+                    ZStack {
+                        /*Rectangle()
+                            .frame(height: 30)
+                            .foregroundColor(.black)*/
+                        
+                        Rectangle()
+                            .frame(height: 30)
+                        .foregroundColor(sound.color)
+                    }
+                    
+                }
+                
+            }.padding(.horizontal)
+            
+            List{
+                ForEach(sounds) { sound in
+                    HStack {
+                        Spacer()
+                        Button {
+                            print("sound played")
+                        } label: {
+                            ZStack {
+                                Rectangle()
+                                    .foregroundColor(sound.color)
+                                    .frame(width: 188, height: 30)
+                                    .cornerRadius(10)
+                                
+                                Text(sound.name)
+                                    .foregroundColor(.black)
+                            }
+                    }
+                        Spacer()
+                    }
+                }.onMove(perform: move)
+
+            }
+            .listStyle(PlainListStyle())
+            .frame(height: 300)
+            .padding(.vertical, 40)
+            
+            Button {
+                showModal=true
+            } label: {
+                ZStack {
+                    Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(width: 188, height: 30)
+                    .background(Color(red: 0.87, green: 0.87, blue: 0.87))
+                    .cornerRadius(10)
+                    
+                    Image(systemName: "plus")
+                        .foregroundColor(.black)
+                }
+            }
+
+            
+            Button(action: {
+                //open alert to save sound
+            }, label: {
+                Image(systemName: "square.and.arrow.down")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 40)
+                    .foregroundColor(.green)
+            })
+            .padding(.vertical, 50)
+            
+        }
+        .sheet(isPresented: $showModal, onDismiss: {
+            sounds=data.mixSounds
+        }, content: {
+            ModalView()
+        })
     }
 }
+
 
 #Preview {
     MixingView()
