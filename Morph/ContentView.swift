9f0233d9d820c9a3e2eae03f821a2da198015314
Marziap <marz.pirozzi@studenti.unina.it>
2023-10-20 14:49:41 +0200

    Initial Commit

---
+++ Morph/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Morph
+//
+//  Created by Marzia Pirozzi on 20/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Morph/MorphApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorphApp.swift
+//  Morph
+//
+//  Created by Marzia Pirozzi on 20/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MorphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
