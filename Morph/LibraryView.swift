5e953a8cfd1e1a48f5e61e04deeb9b28a0f284f5
gcaiazzo9 <giorgiocaiazzo@icloud.com>
2023-10-24 14:09:36 +0200

    Library edit

--- Morph/LibraryView.swift
+++ Morph/LibraryView.swift
@@ -21,7 +21,7 @@ struct LibraryView: View {
                 
                 Picker("in what category are you?", selection: $category) {
                     Text("Sounds").tag(0)
-                    Text("Music").tag(1) }
+                    Text("Music").tag(1)}
                 .pickerStyle(.segmented)
                 .frame(width: 200)
                 .padding()
@@ -52,19 +52,21 @@ struct LibraryView: View {
                             .swipeActions (edge:.trailing, allowsFullSwipe: false) {
                                 
                                 Button(role: .destructive) {
-                                    print("Deleting conversation")
+                                    print("Deleting sound")
                                 } label: {
                                     Label("Delete", systemImage: "trash.fill")
                                 }
                             }
                             .swipeActions (edge:.leading, allowsFullSwipe: false) {
                                 
-                                Button {
-                                print("Edit screen")
-                                } label: {Label("Edit", systemImage: "slider.vertical.3")
-                                        .tint(.green)
+                                NavigationLink (destination: MorphEditScreen()) {
+                                    Button {
+                                        print("Edit screen")
+                                    } label: {Label("Edit", systemImage: "slider.vertical.3")
+                                            .tint(.green)
                                     }
-                             }
+                                }
+                            }
                             
                         }
                 }
@@ -75,8 +77,21 @@ struct LibraryView: View {
                 
                 HStack {
                     
-                    customButton (buttonImage: "mic")
-                    customButton (buttonImage: "paperclip")
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Image (systemName: "mic")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 30)
+                            .padding()
+                    })
+                         
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Image (systemName: "paperclip")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 40)
+                            .padding(.horizontal)
+                    })
                     
                     
                 }
